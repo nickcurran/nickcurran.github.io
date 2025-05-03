63865b9a07e000cd3d6179416f749478daac732b
Nick Curran <nick@tcob.com>
2025-05-03 17:37:44 -0500

    Fix load glitch by turning off SSR for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+import dynamic from 'next/dynamic'
+
 import Flipper from "@/components/Flipper";
-import Banner from '@/components/Banner'
 import About from '@/components/About'
+
+// Prevents visual glitch on load if server side and client side
+// random title are different
+// https://nextjs.org/docs/app/guides/lazy-loading#skipping-ssr
+const Banner = dynamic(() => import('@/components/Banner'), { ssr: false })
 
 export default function Home() {
     return (
