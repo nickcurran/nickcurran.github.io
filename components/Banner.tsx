bd6b7c813c9effe7cacd01adcf68a0d7fdc8fe85
Nick Curran <nick@tcob.com>
2025-05-03 11:26:12 -0500

    Cleaner flipping

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -15,23 +15,20 @@ function getRandomInt(max: number) {
 
 export default function Banner() {
 
-    const [isFlipped, setIsFlipped] = useState(false)
+    const [flipClass, setFlipClass] = useState("")
 
     const index = getRandomInt(Titles.titles.length)
     const title = Titles.titles[index]
     const words = title.split(" ")
 
-    const handleClick = () => {
-        setIsFlipped(!isFlipped)
-        const front = document.getElementById("banner")
-        if (front) {
-            front.classList.toggle("rotate-y-180")
-        }
+    function flip() {
+        setFlipClass(flipClass == "" ? "rotate-y-180" : "")
     }
 
     return (
-        <div id="container" onClick={handleClick} className='absolute perspective-distant top-1/2 left-1/2 bg-red-500 -translate-x-1/2 -translate-y-1/2'>
-            <div id="banner" className={`transform-3d duration-[1s] transition-transform ${outfit.className} bg-green-500 text-[15vw] leading-[85%]`}>
+        <div id="container" onClick={flip} className='absolute perspective-distant top-1/2 left-1/2 bg-red-500 -translate-x-1/2 -translate-y-1/2'>
+
+            <div id="flipper" className={`${flipClass} transform-3d duration-[1s] transition-transform ${outfit.className} bg-green-500 text-[15vw] leading-[85%]`}>
 
                 <div id="front" className="bg-yellow-500 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 backface-hidden">
                     {words.map((word, i) =>
