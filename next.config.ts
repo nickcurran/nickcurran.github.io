bc6ab14be7c87ac837ba7c61183818621a3b32ff
Nick Curran <nick@tcob.com>
2025-05-04 18:27:35 -0500

    Fixed turbopack warning?

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     // Configure `pageExtensions` to include markdown and MDX files
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
     // Optionally, add any other Next.js config below
+
+    turbopack: {
+        resolveExtensions: ['.js', '.jsx', '.ts', '.tsx', '.md', '.mdx'],
+    }
 };
 
 const withMDX = createMDX({
