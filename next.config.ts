9af8e5910fabbba8a2359a8f91a51d3d9d590dd3
Nick Curran <nick@tcob.com>
2025-05-04 19:33:52 -0500

    Another attempt

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,16 @@ import type { NextConfig } from "next";
 import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
+    // output: 'export' must be here for github pages
+    output: 'export',
+
     // Configure `pageExtensions` to include markdown and MDX files
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    
     // Optionally, add any other Next.js config below
-    // turbopack: {
-    //     resolveExtensions: ['.js', '.jsx', '.ts', '.tsx', '.md', '.mdx'],
-    // }
+    turbopack: {
+        resolveExtensions: ['.js', '.jsx', '.ts', '.tsx', '.md', '.mdx'],
+    }
 };
 
 const withMDX = createMDX({
