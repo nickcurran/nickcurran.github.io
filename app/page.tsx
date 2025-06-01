184bfdbbcfd2b20ac4b8803d8033cc9d958f9ab2
Nick Curran <nick@tcob.com>
2025-05-31 19:22:24 -0500

    Getting font to work

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import { ReactElement } from 'react'
 import Link from 'next/link'
 
+import { Outfit } from 'next/font/google'
+ 
+const outfit = Outfit({
+  subsets: ['latin'],
+})
 export default function Home(): ReactElement {
   const title = 'Taking Care of Business'
 
   return (
-    <h1 id='banner'>
+    <h1 id='banner' className={outfit.className}>
       <Link href='/about' className='grow-0'>
         {title.split(' ').map((word, i) =>
           <div key={i++}>{word}</div>
